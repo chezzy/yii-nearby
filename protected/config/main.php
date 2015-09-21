774e8acbbdf7fd34ddec7b4b247ef4ca020adb95
Dizzy <ches@ukr.net>
2015-09-21 16:30:41 +0300

    Added parks.json file with some data for Google API

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,19 +6,19 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Places Nearby',
-	'import'=>array(
+	'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name' => 'Places Nearby',
+	'import' => array(
 		'application.models.*',
 	),
-	'components'=>array(
-		'db'=>array(
+	'components' => array(
+		'db' => array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/locations.db',
 		),
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'showScriptName'=>false,
-			'rules'=>array(
+		'urlManager' => array(
+			'urlFormat' => 'path',
+			'showScriptName' => false,
+			'rules' => array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
